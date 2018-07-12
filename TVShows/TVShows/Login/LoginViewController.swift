17652a504e31827bc92ca79c2cf067defffc3064
Ivana Mrsic <ivana.mrsic@fer.hr>
2018-07-12 09:59:57 +0200

    added functionalities to loginViewController

--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -10,9 +10,57 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBAction func addOneTapped(_ sender: Any) {
+        self.tapCounter += 1
+        tapCounterLabel.text = String(tapCounter)
+        print("tap")
+    }
+    @IBOutlet weak var secondsLeftLabel: UILabel!
+    
+    @IBOutlet weak var toggleIndicatorButton: UIButton!
+    @IBOutlet weak var tapCounterLabel: UILabel!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+
+    
+    @IBAction func toggleIndicatorTapped(_ sender: Any) {
+        self.toggleIndicator()
+    }
+    
+    var timer: Timer = Timer()
+    
+    var indicatorToggler: Bool = true
+    var tapCounter: Int = 0
+    var timeRemaining: Int = 3
+    
+    func toggleIndicator() {
+        if (!self.indicatorToggler) {
+            activityIndicator.startAnimating()
+        } else {
+            activityIndicator.stopAnimating()
+        }
+        self.indicatorToggler = !self.indicatorToggler
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        activityIndicator.startAnimating()
+        toggleIndicatorButton.layer.cornerRadius = 25
+        tapCounterLabel.text = String(tapCounter)
+        timeRemaining = 3
+        secondsLeftLabel.text = String(timeRemaining)
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timerRunning), userInfo: nil, repeats: true)
+    }
+    
+    @objc
+    func timerRunning() {
+        timeRemaining -= 1
+        secondsLeftLabel.text = String(timeRemaining)
+        
+        if(timeRemaining == 0) {
+            timer.invalidate()
+            toggleIndicator()
+        }
     }
 }
