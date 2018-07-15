72177b35f682167fbfd2afc78b9097e60f7e183c
Ivana Mrsic <ivana.mrsic@fer.hr>
2018-07-15 16:45:54 +0200

    removed storyboard, added xib file

--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -2,7 +2,7 @@
 //  LoginViewController.swift
 //  TVShows
 //
-//  Created by Ivana Mrsic on 12/07/2018.
+//  Created by Ivana Mrsic on 15/07/2018.
 //  Copyright © 2018 Ivana Mrsic. All rights reserved.
 //
 
@@ -10,56 +10,26 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
-    @IBOutlet weak var secondsLeftLabel: UILabel!
-    @IBOutlet weak var toggleIndicatorButton: UIButton!
-    @IBOutlet weak var tapCounterLabel: UILabel!
-    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-
-    var timer: Timer = Timer()
-    
-    var indicatorToggler: Bool = true
-    var tapCounter: Int = 0
-    var timeRemaining: Int = 3
-    
-    @IBAction func toggleIndicatorTapped(_ sender: Any) {
-        self.toggleIndicator()
-    }
-    
-    @IBAction func addOneTapped(_ sender: Any) {
-        self.tapCounter += 1
-        tapCounterLabel.text = String(tapCounter)
-        print("tap")
-    }
-    
-    func toggleIndicator() {
-        if (!self.indicatorToggler) {
-            activityIndicator.startAnimating()
-        } else {
-            activityIndicator.stopAnimating()
-        }
-        self.indicatorToggler = !self.indicatorToggler
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        timeRemaining = 3
-        secondsLeftLabel.text = String(timeRemaining)
-        activityIndicator.startAnimating()
-        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timerRunning), userInfo: nil, repeats: true)
-        
-        toggleIndicatorButton.layer.cornerRadius = 25
-        tapCounterLabel.text = String(tapCounter)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
-    @objc
-    func timerRunning() {
-        timeRemaining -= 1
-        secondsLeftLabel.text = String(timeRemaining)
-        
-        if(timeRemaining < 1) {
-            timer.invalidate()
-            toggleIndicator()
-        }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
     }
+    */
+
 }
