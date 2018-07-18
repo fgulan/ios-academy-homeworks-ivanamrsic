29a1ccfd234d073e7c0daeeb17528dd809871a4e
Ivana Mrsic <ivana.mrsic@fer.hr>
2018-07-18 19:19:03 +0200

    changed appdelete for last pull request comments

--- TVShows/TVShows/AppDelegate.swift
+++ TVShows/TVShows/AppDelegate.swift
@@ -17,10 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
    
-        let vc = LoginViewController()
-        let nvc = UINavigationController(rootViewController: vc)
-        nvc.navigationBar.isTranslucent = false
-        window?.rootViewController = nvc
+        let loginViewController = LoginViewController()
+        let navigationViewController = UINavigationController(rootViewController: loginViewController)
+        navigationViewController.navigationBar.isTranslucent = false
+        window?.rootViewController = navigationViewController
         window?.makeKeyAndVisible()
         return true
     }
