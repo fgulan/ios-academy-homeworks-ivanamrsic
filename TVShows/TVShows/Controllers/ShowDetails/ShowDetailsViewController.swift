f3cd5eb71fd21655a2d81abac4e9cb6a210e1ac1
Ivana Mrsic <ivana.mrsic@fer.hr>
2018-07-25 17:54:28 +0200

    code reogranization, added new showdetails view controller

---
+++ TVShows/TVShows/Controllers/ShowDetails/ShowDetailsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ShowDetailsViewController.swift
+//  TVShows
+//
+//  Created by Ivana Mrsic on 25/07/2018.
+//  Copyright © 2018 Ivana Mrsic. All rights reserved.
+//
+
+import UIKit
+
+class ShowDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
